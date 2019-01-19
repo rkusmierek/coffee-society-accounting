588f3c1cccd83c7eb7b5859f216deb54966b08a7
khelman <kamil.helman@altkom.pl>
2019-01-19 21:53:18 +0100

    Unify api URIs

--- accounting-web/src/main/kotlin/pl/altkom/coffee/accounting/web/controller/AccountingSearchController.kt
+++ accounting-web/src/main/kotlin/pl/altkom/coffee/accounting/web/controller/AccountingSearchController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/search")
+@RequestMapping("/api/accounting")
 class AccountingSearchController(private val queryGateway: QueryGateway) {
 
 }
